d8f8867eeff8378888bbc604407acc9a2c1f79ac
Lukas323 <lukas_kennedy@brown.edu>
2023-03-14 13:26:25 -0400

    wow finally did it

--- ProAvailability.java
+++ ProAvailability.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * ProAvailability class instantiates an Array of type Input.
@@ -93,7 +94,15 @@ public class ProAvailability {
 
         System.out.println(Arrays.deepToString(this.createBusinessHours()));
 
+        //I want to say for every array in Integer[][] convert it to the proper form
+
         this.makeString(integerBusinessHours[0][0]);
+
+
+
+//        String integerBusinessHours.toString();
+
+
 
 //        for(Integer[] array: integerBusinessHours){
 //            array[0]
@@ -107,11 +116,62 @@ public class ProAvailability {
      */
     public void makeString(Integer integer){
         String intString = integer.toString();
+
         if(intString.length() == 3){
             intString = 0 + intString;
         }
+        //turn a colon into a character
+        char colon = Character.valueOf(':');
+
+        //add colon to the string at the proper index
+        String newIntString = this.addChar(intString, colon, 2);
+
+        System.out.println(newIntString);
+
+
+
+//        char[] charArray = intString.toCharArray();
+//        char[] newCharArray = new char[4];
+//        for(int i = 0; i < 4; i++){
+//            char character = charArray[i];
+//            if(i == 1){
+//                character = character + ":";
+//            }
+//            newCharArray[i] = character;
+//        }
+//        System.out.println(newCharArray);
 
         //add semicolon
-        System.out.println(intString);
+    }
+
+    /**
+     * addChar method takes in a string within which to add the character,
+     * the character to add, and the position to add it at.
+     * @param string
+     * @param character
+     * @param position
+     * @return
+     */
+    public String addChar(String string, char character, int position){
+        //initialize the strings length
+        int length = string.length();
+
+        //create a character array with a space to add the new character
+        char[] updatedArray = new char[length + 1];
+
+        /*copy string char from index 0 to position of insertion,
+        add copied characters to updatedArray. Copy these characters
+        into updatedArray starting at index 0 of updatedArray
+         */
+        string.getChars(0, position, updatedArray, 0);
+
+        updatedArray[position] = character;
+
+        /*then get string characters from the position onward and copy them into
+          the updatedArray, starting at position + 1
+         */
+        string.getChars(position, length, updatedArray, position + 1);
+
+        return new String(updatedArray);
     }
 }
