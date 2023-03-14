83cff2697312ef16b88dd65aa2f23f7f588634d5
Lukas323 <lukas_kennedy@brown.edu>
2023-03-14 09:08:07 -0400

    test suite

--- FileParserTestSuite.java
+++ FileParserTestSuite.java
@@ -1,7 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * public class FileParserTestSuite serves
  * as the testing class for the FileParser class
  * and methods.
  */
 public class FileParserTestSuite {
+
+    @Before
+    public void setupTestSuite(){
+
+    }
+
+    @Test
+    public void testDuplicateInputs(){
+
+    }
+
+    @Test
+    public void testEmptyLines(){
+
+    }
 }
